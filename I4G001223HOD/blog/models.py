86b2a4268d4344e58642e5902833f055569b0b8c
Efezino Idisi <enduranceidisi13@gmail.com>
2022-06-17 00:41:49 +0100

    create a django blog app and add model

---
+++ I4G001223HOD/blog/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_date = models.DateTimeField('date created')
+    published_date = models.DateTimeField('date published')
+    
+    
+    def __str__(self):
+        return self.title
